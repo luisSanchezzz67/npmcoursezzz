dbd6e75f6b6a51cb80f7b4f5af437dd366263758
luis cruz <luiscrruzsanch3837@gmail.com>
2022-05-12 12:21:31 -0500

    primer version de npm

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Se declara el arreglo
+const messages = [
+    "David",
+    "Diana",
+    "carolina",
+    "paulina",
+    "Ana Maria",
+    "Isabela",
+    "Antonio",
+    "Norma"
+]
+
+//Crear función para enviar aleatoriamente  los nombres del arreglo
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()*messages.length)]
+    console.log(message)
+}
+
+// Exportar como un módulo
+
+module.exports = { randomMsg }
